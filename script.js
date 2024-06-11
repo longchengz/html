520cbbd1dd299e55a2736a74bc26153ce4915903
Y G <143701265+longchengz@users.noreply.github.com>
2024-06-11 15:28:05 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The page is fully loaded.');
+    // Additional JavaScript code can be added here
+});
